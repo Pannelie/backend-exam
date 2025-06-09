fcb85b198de13bdb549a70ca9ea30af364d4c047
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-06-09 14:14:55 +0200

    used bcrypt to save hashedPassword for registration of new user

--- startkod/services/users.js
+++ startkod/services/users.js
@@ -1,22 +1,26 @@
-import User from '../models/user.js';
+import bcrypt from "bcrypt";
+import User from "../models/user.js";
 
 export async function registerUser(user) {
-    try {
-        const result = await User.create(user);
-        return result;
-    } catch(error) {
-        console.log(error.message);
-        return null;
-    }
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    const newUser = { ...user, password: hashedPassword };
+
+    const result = await User.create(newUser);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 }
 
 export async function getUser(username) {
-    try {
-        const user = await User.findOne({ username : username});
-        if(user) return user;
-        else throw new Error('No user found');
-    } catch(error) {
-        console.log(error.message);
-        return null;
-    }
-} 
+  try {
+    const user = await User.findOne({ username: username });
+    if (user) return user;
+    else throw new Error("No user found");
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
